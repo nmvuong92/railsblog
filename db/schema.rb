fb8d5f8012122763ea53bdc0ba50161fcef9ee5c
charlie <charlie.nguyen.goldenowl@gmail.com>
2023-01-26 11:04:55 +0700

    remove tailwindcss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_26_035622) do
+ActiveRecord::Schema.define(version: 2023_01_26_035730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 2023_01_26_035622) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
--- spec/models/widget_spec.rb
+++ spec/models/widget_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Widget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has none to begin with" do
+    expect(Widget.count).to eq 0
+  end
+
+  it "has one after adding one" do
+    Widget.create
+    expect(Widget.count).to eq 1
+  end
+
+  it "has none after one was created in a previous example" do
+    expect(Widget.count).to eq 0
+  end
 end
