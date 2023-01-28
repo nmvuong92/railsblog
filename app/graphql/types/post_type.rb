6783195eb70848508f09b0b106c86e1fea877c42
charlie <charlie.nguyen.goldenowl@gmail.com>
2023-01-28 17:36:52 +0700

    init graphql

---
+++ app/graphql/types/post_type.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Types
+  class PostType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String
+    field :title, String
+    field :content, String
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :title, String
+    field :rating, Integer
+  end
+end
